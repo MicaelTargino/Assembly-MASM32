de4bc0d48f675e78decd5dbe152160444ea198ed
MicaelTargino <micaelgomestargino@gmail.com>
2024-10-05 14:18:27 -0300

    integrate OP operations

--- print_multiples_of_11_with_rest_5/print_multiples_of_11_w_rest_5.asm
+++ print_multiples_of_11_with_rest_5/print_multiples_of_11_w_rest_5.asm
@@ -5,17 +5,24 @@ option casemap :none                ; Make labels and variable names case-sensit
 ; Include necessary headers for Windows API and MSVCRT (C runtime) functions.
 include \masm32\include\windows.inc
 include \masm32\include\kernel32.inc
-include \masm32\include\msvcrt.inc
+include \masm32\include\masm32.inc
 includelib \masm32\lib\kernel32.lib
-includelib \masm32\lib\msvcrt.lib
-include \masm32\macros\macros.asm
+includelib \masm32\lib\masm32.lib
 
 .data                               ; Start of the data segment for variables.
     start_value dd 1000             ; Initialize `start_value` with 1000 (starting value for the loop).
     divisor dd 11                   ; Define the divisor (11) for the division operations.
+    outputString db 50 dup(0)
+    outputHandle dd 0
+    console_count dd 0
+    tam_string dd 0
 
 .code                               ; Start of the code segment where the logic is implemented.
 start:
+    ; Get the output handle (keyboard) using GetStdHandle API function.
+    invoke GetStdHandle, STD_OUTPUT_HANDLE
+    mov outputHandle, eax  ; Store the output handle in the outputHandle variable.
+
     ; Load the initial value `start_value` (1000) into `ebx` (the current value to check in the loop).
     MOV ebx, dword ptr [start_value]
 
@@ -45,15 +52,55 @@ loopStart:
     CMP edx, 5
     JNE skip                        ; If `edx != 5`, jump to `skip` label.
 
-    ; If the remainder is 5, print the current value of `ebx`.
-    printf("%d\n", ebx)
+    ; Backup EAX, ECX, EDX before calling dwtoa
+    push eax
+    push ecx
+    push edx
+    invoke dwtoa, ebx, offset outputString
+    ; Restore EAX, ECX, EDX after dwtoa
+    pop edx
+    pop ecx
+    pop eax
+
+    ; Backup EAX, ECX, EDX before calling StrLen
+    push eax
+    push ecx
+    push edx
+    invoke StrLen, addr outputString
+    mov tam_string, eax  ; Store the string length in `tam_string`
+    
+    ; Load the base address of outputString into ESI
+    lea esi, [outputString]
+    
+    ; Add tam_string to ESI to get the next available position in the string
+    add esi, tam_string
+    mov byte ptr [esi], 32               ; Add a space after the number
+    inc esi                                ; Move to the next byte
+    mov byte ptr [esi], 0                  ; Add null terminator after the space
+    
+    ; Restore EAX, ECX, EDX after StrLen
+    pop edx
+    pop ecx
+    pop eax
+
+    ; Backup EAX, ECX, EDX before calling WriteConsole
+    push eax
+    push ecx
+    push edx
+    ADD dword ptr [tam_string], 1
+    invoke WriteConsole, outputHandle, addr outputString, tam_string, addr console_count, NULL
+    ; Restore EAX, ECX, EDX after WriteConsole
+    pop edx
+    pop ecx
+    pop eax
 
 skip:
     ; Increment `ebx` to check the next number.
     INC ebx
 
-    ; Decrement the loop counter `ecx` and repeat the loop if `ecx` is not zero.
-    LOOP loopStart                  ; Loop back to `loopStart` until `ecx == 0`.
+    ; Decrement the loop counter and repeat the loop if `ecx` is not zero.
+    dec ecx                     ; Decrement the loop counter manually.
+    JNZ loopStart               ; Jump back to `loopStart` if `ecx` is not zero.
 
 exitTheProgram:
     ; Exit the program.
